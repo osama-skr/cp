1799b608a8878bae96b19c7527645e292256c07d
Meligy122 <meligyahmed122@gmail.com>
2025-08-02 14:03:52 +0300

    Create Pascal triangle.cpp

---
+++ Math/Cobinatories/Ncr/Pascal triangle.cpp
@@ -0,0 +1,12 @@
+const int M = 1000;
+const int MOD = 1000000007;
+int C[M + 1][M + 1];
+
+void PreCalculation() {
+    C[0][0] = 1;
+    for (int n = 1; n <= M; ++n) {
+        C[n][0] = C[n][n] = 1;
+        for (int k = 1; k < n; ++k)
+            C[n][k] = (C[n - 1][k - 1] + C[n - 1][k]) % MOD;
+    }
+}
