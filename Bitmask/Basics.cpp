4121f1ac708f6c5898d892c459f2273066a637cf
Meligy122 <meligyahmed122@gmail.com>
2025-08-04 19:37:42 +0300

    Create Basics.cpp

---
+++ Bitmask/Basics.cpp
@@ -0,0 +1,26 @@
+// check i-th bit (0-indexed)
+(mask >> i) & 1;
+
+// set i-th bit to 1
+mask |= (1 << i);
+
+// set i-th bit to 0
+mask &= ~(1 << i);
+
+//(union)
+maskA | maskB;
+
+//(intersection)
+maskA & maskB;
+
+// number of set bits
+__builtin_popcount(mask);           // int
+__builtin_popcountll(mask);         // long long
+
+// returns idx for first one from right (0-indexed)
+__builtin_ctz(mask);                // int
+__builtin_ctzll(mask);              // long long
+
+// returns idx for first one from left (0-indexed)
+31 - __builtin_clz(mask);           // int
+63 - __builtin_clzll(mask);         // long long
